6ba8405e383d0a3b9c53cb5e7ab57d46f50ea914
iamakshu29 <vermaakshat420@gmail.com>
2026-08-21 14:56:02 +0530

    chore: fix comment indentation and add a secrets manager deletion command reference

--- acm.tf
+++ acm.tf
@@ -11,10 +11,12 @@ resource "aws_acm_certificate" "strata" {
   tags = merge({ Name = "strata-certificate" }, local.tags)
 }
 
-  # Validation uses the Route 53 records created in route53.tf
-  # Uncomment and apply again when u have proper dns with nameservers.
+# Validation uses the Route 53 records created in route53.tf
+# Uncomment and apply again when u have proper dns with nameservers.
 # resource "aws_acm_certificate_validation" "strata" {
 #   certificate_arn         = aws_acm_certificate.strata.arn
 #   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 # }
 
+# Command
+# aws secretsmanager delete-secret --region ap-south-1 --secret-id starta_secrets_manager s--force-delete-without-recovery
